c9309b7124594ca29247958615f32e451a75809e
JeffreyJoel <jowoloko@gmail.com>
2024-05-13 02:04:36 +0100

    update folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,38 @@
-"use client"
-import GameCard from "@/components/common/GameCard"
-import {games} from "../components/common/Constants"
-import NavBar from "@/components/common/Navbar"
-import Footer from "@/components/common/Footer"
+"use client";
+import { Button } from "@/components/ui/button";
 
-
-export default function page() {
+export default function Home() {
   return (
     <>
-  
-    <div className="bg-homeBack">
+      <section className="container mt-0 sm:mt-6">
+        <div className="mx-auto grid max-w-screen-2xl px-4 pt-20 lg:grid-cols-12 lg:gap-8  lg:pt-28 xl:gap-0">
+          <div className="mr-auto place-self-center  lg:col-span-7">
+            <h1 className="mb-4 max-w-3xl text-4xl font-extrabold leading-none tracking-tight text-white lg:text-5xl xl:text-6xl">
+              Lorem ipsum dolor sit amet consectetur
+              <br />
+            </h1>
 
-    
-    <section className="container mx-auto px-4 py-12 md:px-6 lg:py-16 ">
-      <div className="mb-8 text-center">
-        <h1 className="text-3xl font-bold tracking-tight sm:text-4xl text-white">Explore Our Game Library</h1>
-        <p className="mt-4 text-gray-400">
-          Browse through our extensive collection of exciting board games.
-        </p>
-      </div>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {
-          games.map((game, index)=>(
-            
-            <GameCard game={game} key={index}/>
-          ))
-        }
-      </div>
-    </section>
-    </div>
-    <Footer />
+            <p className="md:text-xllg:mb-8 mb-6 max-w-2xl font-mono font-light text-gray-500 lg:text-2xl">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+              quo maiores hic vero asperiores voluptas, explicabo soluta fugit
+              cumque non nemo, earum a corporis delectus, alias dolorum quod
+              dignissimos libero!
+            </p>
+
+            <div className="mb-4 space-y-4 sm:flex sm:space-x-4 sm:space-y-0">
+              <Button
+                variant={"default"}
+                // className="h-12 min-w-[4rem] gap-2 rounded-xl border border-white/10 bg-primary px-4 py-3 font-bold text-foreground lg:min-w-[8rem] lg:rounded-2xl"
+                className="h-12 min-w-[4rem] gap-2 rounded-xl border border-white/10  px-4 py-3 font-bold text-foreground lg:min-w-[8rem] lg:rounded-2xl"
+                translate="no"
+              >
+                Play
+              </Button>
+            </div>
+          </div>
+          <div className=" lg:col-span-5 lg:mt-0 lg:flex"></div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
