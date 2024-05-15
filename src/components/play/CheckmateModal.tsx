c0a9005041e445a77e555e15a195fbaac00778f9
JeffreyJoel <jowoloko@gmail.com>
2024-05-15 02:46:32 +0100

    add image generator

---
+++ src/components/play/CheckmateModal.tsx
@@ -0,0 +1,50 @@
+// /* eslint-disable react/no-unescaped-entities */
+// import React from "react";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogDescription,
+//   DialogFooter,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogTrigger,
+// } from "@/components/ui/dialog";
+
+
+// interface CheckmateModalProps {
+//     level:String,
+    
+// }
+
+// const CheckmateModal = () => {
+//     const [open, setOpen] = useState(false);
+
+//     function openModal(){
+//         setOpen(false)
+//       }
+
+//   return (
+//     <Dialog>
+//       <DialogTrigger asChild>
+//         <Button variant="outline">Edit Profile</Button>
+//       </DialogTrigger>
+//       <DialogContent className="sm:max-w-[425px]">
+//         <DialogHeader>
+//           <DialogTitle>You won!!</DialogTitle>
+//           <DialogDescription>
+//             Make changes to your profile here. Click save when you're done.
+//           </DialogDescription>
+//         </DialogHeader>
+//         <div className="grid gap-4 py-4">
+     
+//         </div>
+//         <DialogFooter>
+//           <Button type="submit">Save changes</Button>
+//         </DialogFooter>
+//       </DialogContent>
+//     </Dialog>
+//   );
+// };
+
+// export default CheckmateModal;
